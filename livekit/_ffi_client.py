aa75e0bb1e6c70dbb22ee962da6da46b41313f83
rocobo <rodrigo.cobo@oversonicrobotics.com>
2024-10-10 12:13:30 +0200

    [MULTIPROCESS] `fix` wip

--- livekit/_ffi_client.py
+++ livekit/_ffi_client.py
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from importlib_resources import files
-import importlib.resources
+from importlib_resources import files, as_file
 import signal
 import asyncio
 from contextlib import ExitStack
@@ -51,9 +50,9 @@ def get_ffi_lib():
                 Set LIVEKIT_LIB_PATH to specify a the lib path"
         )
 
-    path = files("livekit.resources") / libname
-    # ctx = importlib.resources.as_file(res)
-    # path = _resource_files.enter_context(ctx)
+    res = files("livekit.resources") / libname
+    ctx = as_file(res)
+    path = _resource_files.enter_context(ctx)
     return ctypes.CDLL(str(path))
 
 
